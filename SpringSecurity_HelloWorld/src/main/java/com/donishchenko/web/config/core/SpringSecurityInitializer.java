d117f82da685bb4ed9799fb1244952f68d619b5d
Dmitry Onyshchenko <sacr8tum@gmail.com>
2015-10-22 17:54:50 +0300

    Added SpringSecurity_HelloWorld

---
+++ SpringSecurity_HelloWorld/src/main/java/com/donishchenko/web/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,10 @@
+package com.donishchenko.web.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Security Filter
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //do nothing
+}
